d3a209414ae43fa95ef68eb09fbf87fed91091f0
chen shu ting <a.0066715@gmail.com>
2021-10-08 12:55:39 +0800

    新增monolog/monolog套件

--- public/index.php
+++ public/index.php
@@ -3,9 +3,23 @@ require '../vendor/autoload.php';
 use \Demo\HelloWorld as World;
 use Demo\Hello\Lara;
 use Demo\Hello;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 $world = new World();
+echo '<br>';
 $lara= new Lara();
+echo '<br>';
 $vincent= new Hello\Someone('Vincent');
-
+echo '<br>';
 $mary= new \Demo\Hello\Someone('Mary');
+echo '<br>';
 $john= new Demo\Hello\Someone('John');
+echo '<br>';
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('path/to/your.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
