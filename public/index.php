2af7490260f68b4898388464c1bec56287b002f5
chen shu ting <a.0066715@gmail.com>
2021-10-08 12:59:11 +0800

    新增nesbot/carbon套件

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ use Demo\Hello\Lara;
 use Demo\Hello;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Carbon\Carbon;
 $world = new World();
 echo '<br>';
 $lara= new Lara();
@@ -23,3 +24,5 @@ $log->pushHandler(new StreamHandler('path/to/your.log', Logger::WARNING));
 // add records to the log
 $log->warning('Foo');
 $log->error('Bar');
+
+printf("現在日期時間: %s", Carbon::now());
