790709e7b7d8c49cb419d0ecbecc0e3c057abea4
chen shu ting <a.0066715@gmail.com>
2021-10-25 10:46:53 +0800

    加入carbon測試程式碼

--- public/index.php
+++ public/index.php
@@ -25,4 +25,19 @@ $log->pushHandler(new StreamHandler('path/to/your.log', Logger::WARNING));
 $log->warning('Foo');
 $log->error('Bar');
 
-printf("現在日期時間: %s", Carbon::now());
+printf("現在時間是:%s", Carbon::now()->toDateTimeString());
+echo '<br>';
+printf("溫哥華現在時間是:%s", Carbon::now('America/Vancouver'));  
+echo '<br>';
+//implicit __toString()
+$tomorrow = Carbon::now()->addDay();
+$lastWeek = Carbon::now()->subWeek();
+$nextSummerOlympics = Carbon::createFromDate(2016)->addYears(4);
+
+$officialDate = Carbon::now()->toRfc2822String();
+
+$howOldAmI = Carbon::createFromDate(1975, 5, 21)->age;
+
+$noonTodayLondonTime = Carbon::createFromTime(12, 0, 0, 'Europe/London');
+
+$internetWillBlowUpOn = Carbon::create(2038, 01, 19, 3, 14, 7, 'GMT');
